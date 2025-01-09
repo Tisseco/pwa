d1a002d1edf5cf307f467204138ba2655d8eca9f
fansoa <fverin.contactpro@gmail.com>
2025-01-09 23:42:32 +0100

    feat: call loginUseCase on mutation

--- src/domains/auth/presenter/components/LoginForm/hooks/useLoginForm/index.tsx
+++ src/domains/auth/presenter/components/LoginForm/hooks/useLoginForm/index.tsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
+import { useMutation } from "@tanstack/react-query"
+import { LoginPayload } from "@/domains/auth/domain/types/loginTypes"
+import { useRouteContext } from "@tanstack/react-router"
 
 const schema = z.object({
   email: z.string().email(),
@@ -10,15 +13,21 @@ const schema = z.object({
 type Schema = z.infer<typeof schema>
 
 export const useLoginForm = () => {
+  const { loginUseCase } = useRouteContext({ from: '__root__' })
   const methods = useForm<Schema>({ defaultValues: {
     email: '',
     password: ''
   }, resolver: zodResolver(schema)})
 
-  const onSubmit = (formData: Schema) => console.log(formData)
-  
+  const { mutate } = useMutation({
+    mutationFn: ({ email, password } : LoginPayload) => loginUseCase({ email, password }),
+    onSuccess: async (data) => {
+      console.log("🚀 ~ useLoginForm ~ data:", data)
+    }
+})
+
   return {
     methods,
-    onSubmit: methods.handleSubmit((formData) => onSubmit(formData)),
+    onSubmit: methods.handleSubmit((formData) => mutate(formData)),
   }
 }
