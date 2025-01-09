f1a614865faaf1644435db069774603e126ac34a
fansoa <fverin.contactpro@gmail.com>
2025-01-09 21:50:33 +0100

    feat: import index.css in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import '@/index.css'
 
 import i18n from './domains/shared/services/i18next/initTranslation'
 import {
