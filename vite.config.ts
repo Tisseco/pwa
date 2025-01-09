4444f0f810a380fc830ac16ecdc08231bceb91cc
fansoa <fverin.contactpro@gmail.com>
2025-01-09 13:45:08 +0100

    feat: init shadcn and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // @ts-expect-error: reference types vitest/config doesn't work in vite v6 unlike v5
-	test: {
+  test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/domains/tests/setupTest.ts',
