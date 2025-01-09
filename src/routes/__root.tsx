2b663e63d0a1afe09817e945af0a0b7f1002a60d
fansoa <fverin.contactpro@gmail.com>
2025-01-10 00:04:00 +0100

    refactor: import PWABadge with alias

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRouteWithContext, Link, Outlet, useRouteContext } from '@tanstack/react-router'
-import PWABadge from '../domains/shared/presenter/components/PWABadge'
+import PWABadge from '@/domains/shared/presenter/components/PWABadge'
 
 import { i18n } from 'i18next'
 import { QueryClient } from '@tanstack/react-query'
@@ -7,30 +7,30 @@ import { AuthRepository } from '@/domains/auth/domain/AuthRepository'
 import { Toaster } from '@/domains/shared/presenter/components/ui/toaster'
 
 export const Route = createRootRouteWithContext<{
-    i18n: i18n
-    queryClient: QueryClient
-    loginUseCase: AuthRepository["login"]
+  i18n: i18n
+  queryClient: QueryClient
+  loginUseCase: AuthRepository["login"]
 }>()({
   component: Root
 })
 
 function Root() {
-    const { i18n: { t } } = useRouteContext({ from: '__root__' })
+  const { i18n: { t } } = useRouteContext({ from: '__root__' })
 
-    return (
-        <>
-            <div className="p-2 flex gap-2">
-                <Link to="/" className="[&.active]:font-bold">
-                    {t('home', { ns: 'glossary'})}
-                </Link>{' '}
-                <Link to="/about" className="[&.active]:font-bold">
-                    {t('about', { ns: 'glossary' })}
-                </Link>
-            </div>
-            <hr />
-            <Outlet />
-            <Toaster />
-            <PWABadge />
-        </>
-    )
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          {t('home', { ns: 'glossary'})}
+        </Link>{' '}
+        <Link to="/about" className="[&.active]:font-bold">
+          {t('about', { ns: 'glossary' })}
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <Toaster />
+      <PWABadge />
+    </>
+  )
 }
