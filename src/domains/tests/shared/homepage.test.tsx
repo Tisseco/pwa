7d0f3cb60ca88f8d54f499c65c3001f38a597e52
fansoa <fverin.contactpro@gmail.com>
2025-01-09 03:27:55 +0100

    feat: implement query client instance in tanstack router context

--- src/domains/tests/shared/homepage.test.tsx
+++ src/domains/tests/shared/homepage.test.tsx
@@ -3,16 +3,28 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 
 import i18n from '../../../domains/shared/services/i18next/initTranslation'
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+  
+const queryClient = new QueryClient()
 
 // Import the generated route tree
 import { routeTree } from '../../../routeTree.gen';
 // Create a new router instance
 const router = createRouter({
     routeTree,
+    Wrap: ({ children }) => (
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    ),
     context: {
-      i18n
+      i18n,
+      queryClient,
     }
-})
+  })
 
 describe('Shared | Integration', () => {
 
