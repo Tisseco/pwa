5ccc147692c3dcd7bf4c8839481f2d4732c9499e
fansoa <fverin.contactpro@gmail.com>
2025-01-10 01:45:37 +0100

    feat: render login page and check if heading is visible

--- src/domains/tests/auth/integration/login.test.tsx
+++ src/domains/tests/auth/integration/login.test.tsx
@@ -1,6 +1,43 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { render, screen } from "@testing-library/react"
 
+import { routeTree } from "@/routeTree.gen"
+import i18n from '@/domains/shared/services/i18next/initTranslation'
+import { InMemoryAuthRepository } from "@/domains/auth/gateways/InMemoryAuthRepository"
+import { loginUseCase } from "@/domains/auth/domain/use-cases/login"
+
+const queryClient = new QueryClient()
 describe('Auth | Integration | Use-cases | Login', () => {
 
-  it('Login form prevents submission and displays error message when invalid input is provided', () => {})
+  it('Login form prevents submission and displays error message when invalid input is provided', async () => {
+    const router = createRouter({
+      routeTree,
+      basepath: '/login',
+      Wrap: ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      ),
+      context: {
+        i18n,
+        queryClient,
+        loginUseCase: loginUseCase(new InMemoryAuthRepository([{
+          id: 1,
+          email: 'fverin.recrutement@gmail.com',
+          password: 'test',
+          role: 'DRIVER',
+          username: 'fansoa',
+          token: 'oat_MTEy.ZmJMWGlXY2dmUkp3WUgzdU5yS0wzYnBuVUc5N2hyRld5bGtMWG5VeDQwMDIxNjMwMDI'
+        }]))
+      }
+    })
+    
+    render(<RouterProvider router={router} />)
+
+    // GIVEN Login page is mounted
+    const loginPage = await screen.findByRole('heading', { name: /welcome to tisseco!/i })
+    expect(loginPage).toBeVisible()
+  })
 
 })
