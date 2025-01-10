5f5170aa02067efbc9ff8a2f777846c0393fb3f4
fansoa <fverin.contactpro@gmail.com>
2025-01-10 04:24:15 +0100

    feat: add login form display an toast error if invalid credentials are provided scenario

--- src/domains/tests/auth/integration/login.test.tsx
+++ src/domains/tests/auth/integration/login.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createRouter, RouterProvider } from "@tanstack/react-router"
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 
 import { routeTree } from "@/routeTree.gen"
 import i18n from '@/domains/shared/services/i18next/initTranslation'
@@ -11,7 +11,7 @@ import userEvent from "@testing-library/user-event"
 const queryClient = new QueryClient()
 describe('Auth | Integration | Use-cases | Login', () => {
 
-  it('Login form prevents submission and displays error message when invalid input is provided', async () => {
+  beforeEach(async () => {
     const router = createRouter({
       routeTree,
       basepath: '/login',
@@ -40,7 +40,9 @@ describe('Auth | Integration | Use-cases | Login', () => {
     // THEN Welcome message should be visible 
     const loginPage = await screen.findByRole('heading', { name: /welcome to tisseco!/i })
     expect(loginPage).toBeVisible()
+  })
 
+  it('Login form prevents submission and displays error message when invalid input is provided', async () => {
     const passwordInput = screen.getByPlaceholderText(/••••••••/i)
     const submitButton = screen.getByRole('button', { name: /log in/i })
 
@@ -53,6 +55,29 @@ describe('Auth | Integration | Use-cases | Login', () => {
     // THEN Error validation should be displayed
     expect(screen.getByText(/your email is invalid/i)).toBeVisible()
     expect(screen.getByText(/your password must contain at least 4 characters/i)).toBeVisible()
+  })
+
+  it('Login form display an toast error if invalid credentials are provided', async () => {
+    const emailInput = screen.getByRole('textbox', { name: /email/i })
+    const passwordInput = screen.getByPlaceholderText(/••••••••/i)
+    const submitButton = screen.getByRole('button', { name: /log in/i })
+    
+    // WHEN Login form is filled with invalid credentials
+    await userEvent.type(emailInput, 'invalid.user@example.com')
+    await userEvent.type(passwordInput, 'wrongpassword')
+    await userEvent.click(submitButton)
+    
+    // THEN Toast error should be displayed
+    const toastError = await screen.findByText(/invalid user credentials/i)
+    expect(toastError).toBeVisible()
+
+    // WHEN Toast error close icon is clicked
+    const status = screen.getByRole('region');
+    await userEvent.click(within(status).getByRole('button'))
+    // await userEvent.keyboard('{Escape}')
+
+    // THEN Toast error should not be displayed
+    expect(toastError).not.toBeInTheDocument()
   })
 
 })
