9bec3b07b55556a3448c69acf8589b160f64a569
fansoa <fverin.contactpro@gmail.com>
2025-01-10 04:22:22 +0100

    feat: mock hasPointerCapture used by @radix-ui/react-toast library to determine pointer interactions, this mock prevent error in integration test

--- src/domains/tests/setupTest.ts
+++ src/domains/tests/setupTest.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom/vitest'
+
+// Explanation:
+// The @radix-ui/react-toast library uses hasPointerCapture to determine pointer interactions (e.g., hover states).
+// JSDOM, used for testing, does not support this API natively, which causes errors in tests.
+// This mock provides a default implementation, returning false to simulate that no element has pointer capture.
+// Writable is set to true, allowing override in specific test scenarios if needed.
+beforeAll(() => {
+  Object.defineProperty(HTMLElement.prototype, 'hasPointerCapture', {
+    value: () => false,
+    writable: true,
+  });
+});
