d922503a6e716c8dcdabf9ae31079d7352f99d2e
fansoa <fverin.contactpro@gmail.com>
2025-01-11 22:53:37 +0100

    fix: address entity

--- src/contexts/shared/entities/address.ts
+++ src/contexts/shared/entities/address.ts
@@ -2,13 +2,14 @@ import { UniqueId } from "@/contexts/shared/entities/shared-kernel"
 
 export type Address = {
   id: UniqueId
-  site_name: string
-  street_number: number
-  street_name: string
+  name: string
+  streetNumber: number
+  streetNumberExtension: string | null
+  streetName: string
   city: string
-  zip_code: number
+  zipCode: number
   department: string
-  state_code: number
+  stateCode: number
   coordinates: {
     latitude: number
     longitude: number
