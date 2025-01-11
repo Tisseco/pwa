644ef433585ec79d63770afe9b9b623896e01faf
fansoa <fverin.contactpro@gmail.com>
2025-01-11 17:01:58 +0100

    feat: add Container entity and its related dependencies

--- src/domains/shared/entities/shared-kernel.ts
+++ src/domains/shared/entities/shared-kernel.ts
@@ -34,3 +34,37 @@ export type SiteType =
 export type AreaSize =
   | 'SMALL'
   | 'LARGE'
+
+export type ContainerStatus =
+  | 'IN SERVICE'
+  | 'UNDER REPAIR'
+  | 'OUT OF SERVICE'
+
+export type ContainerCondition =
+  | 'VERY GOOD CONDITION'
+  | 'GOOD CONDITION'
+  | 'AVERAGE CONDITION'
+  | 'TO CHANGE'
+  | 'TO CHANGE URGENTLY'
+
+export type ContainerKey =
+  | 'KA1-12'
+  | 'KA4'
+  | 'C5'
+  | 'KA17-5'
+  | 'KA16'
+  | 'KA2-8'
+  | 'KA15-6'
+  | 'CF3'
+  | 'PARIS'
+  | 'NONE'
+
+export type ContainerColor =
+  | 'G'
+  | 'M'
+  | 'B'
+  | 'WS'
+
+export type ContainerSize =
+  | 'SMALL'
+  | 'LARGE'
