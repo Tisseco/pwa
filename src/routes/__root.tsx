5d6179dae3cbe6daf58a87c8ba070de56f44d414
fansoa <fverin.contactpro@gmail.com>
2025-01-12 23:51:42 +0100

    feat: remove TanStackRouterDevtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,6 @@ import { AuthRepository } from '@/domains/auth/domain/AuthRepository'
 import PWABadge from '@/domains/shared/presenter/components/PWABadge'
 import { Toaster } from '@/domains/shared/presenter/components/ui/toaster'
 import { AuthState } from '@/domains/auth/store/AuthStore'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRouteWithContext<{
   i18n: i18n
@@ -25,7 +24,6 @@ function Root() {
       <Outlet />
       <Toaster />
       <PWABadge />
-      <TanStackRouterDevtools />
     </>
   )
 }
