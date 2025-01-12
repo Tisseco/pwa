132f3256261da3ea063e9941a337d525091fc251
fansoa <fverin.contactpro@gmail.com>
2025-01-12 02:07:34 +0100

    feat: add token dependencies in getAllScheduledTourDetailsByAssignedUser of ScheduledTourDetailsRepository

--- src/contexts/TourManagement/domain/ScheduledTourDetailsRepository.ts
+++ src/contexts/TourManagement/domain/ScheduledTourDetailsRepository.ts
@@ -1,7 +1,8 @@
+import { Token } from "@/contexts/auth/domain/types/authSharedTypes";
 import { ScheduledTourDetails } from "@/contexts/TourManagement/domain/aggregates/scheduledToursDetails";
 
 export interface ScheduledTourDetailsRepository {
   
-  getAllScheduledTourDetailsByAssignedUser(assignedUser: ScheduledTourDetails["assignedTo"]): Promise<ScheduledTourDetails[] | { errors: { message: string }[] }>
+  getAllScheduledTourDetailsByAssignedUser(token: Token['token']): Promise<ScheduledTourDetails[] | { errors: { message: string }[] }>
 
 }
