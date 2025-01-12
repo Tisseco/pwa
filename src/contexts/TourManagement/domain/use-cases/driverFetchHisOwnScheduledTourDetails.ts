fed8600c2a6da3d8a04e6d33408e2cc770aeaa13
fansoa <fverin.contactpro@gmail.com>
2025-01-12 03:42:48 +0100

    feat: add token param

--- src/contexts/TourManagement/domain/use-cases/driverFetchHisOwnScheduledTourDetails.ts
+++ src/contexts/TourManagement/domain/use-cases/driverFetchHisOwnScheduledTourDetails.ts
@@ -1,3 +1,4 @@
+import { Token } from "@/contexts/auth/domain/types/authSharedTypes";
 import { ScheduledTourDetailsRepository } from "@/contexts/TourManagement/domain/ScheduledTourDetailsRepository";
 
-export const driverFetchHisOwnScheduledTourDetailsUseCase = (tourManagementRepository: ScheduledTourDetailsRepository) => () => tourManagementRepository.getAllScheduledTourDetailsByAssignedUser()
+export const driverFetchHisOwnScheduledTourDetailsUseCase = (tourManagementRepository: ScheduledTourDetailsRepository) => (token: Token['token']) => tourManagementRepository.getAllScheduledTourDetailsByAssignedUser(token)
