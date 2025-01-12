4080f446fa0eaab7c4229440c592b1806c888bc7
fansoa <fverin.contactpro@gmail.com>
2025-01-12 02:16:24 +0100

    feat: integrate driverFetchHisOwnScheduledTourDetailsUseCase in Scheduled Tours page loader

--- src/routes/_isAuthenticated/scheduled-tours.tsx
+++ src/routes/_isAuthenticated/scheduled-tours.tsx
@@ -1,12 +1,27 @@
-import { createFileRoute, useRouteContext } from '@tanstack/react-router'
+import { createFileRoute, useLoaderData, useRouteContext } from '@tanstack/react-router'
+import { queryOptions } from '@tanstack/react-query'
 import { HeaderLayout } from '@/contexts/shared/presenter/Layouts/HeaderLayout'
 
 export const Route = createFileRoute('/_isAuthenticated/scheduled-tours')({
+  loader: ({ context: { queryClient, user, driverFetchHisOwnScheduledTourDetailsUseCase }}) => {
+    // TODO: Find an alternative solution to this workaround.
+    // Verifying the existence of the user token is unnecessary here because a route guard
+    // is already implemented in the `beforeLoad` of my `_isAuthenticated` outlet.
+    if (user?.token) {
+      return queryClient.ensureQueryData(queryOptions({
+        queryKey: ['scheduledTours'],
+          queryFn: () => driverFetchHisOwnScheduledTourDetailsUseCase(user.token),
+          refetchOnWindowFocus: true,
+      }))
+    }
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
   const { i18n: { t } } = useRouteContext({ from:'__root__' })
+  const scheduledTourDetailsList = useLoaderData({ from: '/_isAuthenticated/scheduled-tours' })
+  console.log("🚀 ~ RouteComponent ~ scheduledTourDetailsList:", scheduledTourDetailsList)
   
   return (
     <HeaderLayout 
