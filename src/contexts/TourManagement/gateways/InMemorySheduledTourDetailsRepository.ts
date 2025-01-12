bfab3f2813762785e2efa80596c8bdecb9cbdba6
fansoa <fverin.contactpro@gmail.com>
2025-01-12 02:12:38 +0100

    feat: add currentUserId to simulate backend logic for retrieving user ID from token

--- src/contexts/TourManagement/gateways/InMemorySheduledTourDetailsRepository.ts
+++ src/contexts/TourManagement/gateways/InMemorySheduledTourDetailsRepository.ts
@@ -1,17 +1,22 @@
-import { ScheduledTourDetailsRepository } from "../domain/ScheduledTourDetailsRepository";
-import { ScheduledTourDetails } from "../domain/aggregates/scheduledToursDetails";
+import { User } from "@/contexts/shared/entities/user";
+import { ScheduledTourDetailsRepository } from "@/contexts/TourManagement/domain/ScheduledTourDetailsRepository";
+import { ScheduledTourDetails } from "@/contexts/TourManagement/domain/aggregates/scheduledToursDetails";
 
 export class InMemoryScheduledTourDetailsRepository implements ScheduledTourDetailsRepository {
     
   private dataSource;
+  // currentUserId is here simulate backend logic for retrieving user ID from token
+  private currentUserId;
     
-  constructor(dataSource: ScheduledTourDetails[]){
+  constructor(dataSource: ScheduledTourDetails[], currentUserId: User['id']){
     this.dataSource = dataSource
+    this.currentUserId = currentUserId
   }
-  
-  getAllScheduledTourDetailsByAssignedUser(assignedUserId: ScheduledTourDetails['id']): Promise<ScheduledTourDetails[] | { errors: { message: string }[] }> {
+
+  // Implement token in func parameters in future
+  getAllScheduledTourDetailsByAssignedUser(): Promise<ScheduledTourDetails[] | { errors: { message: string }[] }> {
     return new Promise((resolve) => {
-      const scheduledTourDetailsList = this.dataSource.filter(scheduledTourDetails => scheduledTourDetails.id === assignedUserId)
+      const scheduledTourDetailsList = this.dataSource.filter(scheduledTourDetails => scheduledTourDetails.id === this.currentUserId)
       
       if (scheduledTourDetailsList) return resolve(scheduledTourDetailsList)
       
