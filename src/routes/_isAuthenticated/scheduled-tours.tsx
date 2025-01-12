e32d57072f321db4d43cb6cb01f49889a496d0a2
fansoa <fverin.contactpro@gmail.com>
2025-01-13 00:41:36 +0100

    feat: add errorComponent to /scheduled-tours page

--- src/routes/_isAuthenticated/scheduled-tours.tsx
+++ src/routes/_isAuthenticated/scheduled-tours.tsx
@@ -1,31 +1,79 @@
-import { createFileRoute, useLoaderData, useRouteContext } from '@tanstack/react-router'
-import { queryOptions } from '@tanstack/react-query'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createFileRoute, useRouteContext, useRouter } from '@tanstack/react-router'
+import { queryOptions, useQueryErrorResetBoundary, useSuspenseQuery } from '@tanstack/react-query'
 import { HeaderLayout } from '@/contexts/shared/presenter/Layouts/HeaderLayout'
+import { Token } from '@/contexts/auth/domain/types/authSharedTypes'
+import { ScheduledTourDetailsRepository } from '@/contexts/TourManagement/domain/ScheduledTourDetailsRepository'
+import { useEffect } from 'react'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/contexts/shared/presenter/components/ui/alert-dialog"
+
+const createScheduledTourDetailsQueryOptions = (token: Token['token'], driverFetchHisOwnScheduledTourDetailsUseCase: ScheduledTourDetailsRepository['getAllScheduledTourDetailsByAssignedUser']) => queryOptions({
+  queryKey: ['scheduledTours'],
+  queryFn: () => driverFetchHisOwnScheduledTourDetailsUseCase(token)
+})
 
 export const Route = createFileRoute('/_isAuthenticated/scheduled-tours')({
-  loader: ({ context: { queryClient, user, driverFetchHisOwnScheduledTourDetailsUseCase }}) => {
-    // TODO: Find an alternative solution to this workaround.
-    // Verifying the existence of the user token is unnecessary here because a route guard
-    // is already implemented in the `beforeLoad` of my `_isAuthenticated` outlet.
-    if (user?.token) {
-      return queryClient.ensureQueryData(queryOptions({
-        queryKey: ['scheduledTours'],
-          queryFn: () => driverFetchHisOwnScheduledTourDetailsUseCase(user.token),
-          refetchOnWindowFocus: true,
-      }))
-    }
-  },
+  loader: async ({ context: { queryClient, user, driverFetchHisOwnScheduledTourDetailsUseCase } }) => 
+    // @ts-expect-error: temporary ts exception
+    queryClient.ensureQueryData(createScheduledTourDetailsQueryOptions(user.token, driverFetchHisOwnScheduledTourDetailsUseCase)),
   component: RouteComponent,
+  errorComponent: ({ error }) => {
+    const { i18n: { t } } = useRouteContext({ from: '__root__' })
+    const router = useRouter()
+    const queryErrorResetBoundary = useQueryErrorResetBoundary()
+
+    useEffect(() => {
+      // Reset the query error boundary
+      queryErrorResetBoundary.reset()
+    }, [queryErrorResetBoundary])
+
+    return (
+      <AlertDialog defaultOpen>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{t('common:something.went.wrong')}</AlertDialogTitle>
+            <AlertDialogDescription>
+              {error.message}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>{t('glossary:cancel')}</AlertDialogCancel>
+            <AlertDialogAction onClick={() => {
+              // Invalidate the route to reload the loader, and reset any router error boundaries
+              router.invalidate()
+            }}>
+              {t('common:refresh.app')}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
+  },
 })
 
 function RouteComponent() {
-  const { i18n: { t } } = useRouteContext({ from:'__root__' })
-  const scheduledTourDetailsList = useLoaderData({ from: '/_isAuthenticated/scheduled-tours' })
-  console.log("🚀 ~ RouteComponent ~ scheduledTourDetailsList:", scheduledTourDetailsList)
-  
+  const { i18n: { t }, user, driverFetchHisOwnScheduledTourDetailsUseCase } = useRouteContext({ from:'__root__' })
+
+  // @ts-expect-error: temporary ts exception
+  const { data: scheduledTourDetailsList} = useSuspenseQuery(createScheduledTourDetailsQueryOptions(user.token, driverFetchHisOwnScheduledTourDetailsUseCase))
+
+  if ("errors" in scheduledTourDetailsList) {
+    throw new Error(scheduledTourDetailsList.errors?.[0]?.message)
+  }
+
   return (
     <HeaderLayout 
-      breadcrumbItemList={[
+    breadcrumbItemList={[
         { 
           link: '/',
           label: t('glossary:home'),
@@ -37,7 +85,7 @@ function RouteComponent() {
         },
       ]}
     >
-      <div>Scheduled tours</div>
+      <></>
     </HeaderLayout>
   )
 }
