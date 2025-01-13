dd2c761bd01995369d9d714ffca073a0deeaf1ef
fansoa <fverin.contactpro@gmail.com>
2025-01-13 03:27:26 +0100

    feat: add ScheduledTourCatalog

---
+++ src/contexts/TourManagement/presenter/components/ScheduledTourCatalog.tsx
@@ -0,0 +1,69 @@
+import { Link, useRouteContext } from "@tanstack/react-router"
+import { Badge } from "@/contexts/shared/presenter/components/ui/badge"
+import { Card, CardHeader, CardContent, CardFooter } from "@/contexts/shared/presenter/components/ui/card"
+import { scheduledTourCatalogMap } from "@/contexts/TourManagement/presenter/mappers/scheduledTourCatalogMap"
+
+type TourCardRowProps = {
+    label: string,
+    value: string
+  }
+  
+const TourCardRow = ({ label, value} : TourCardRowProps) => {
+  return (
+    <div className="flex flex-row justify-between">
+      <p className="text-muted-foreground leading-7">{label}</p>
+      <p className="leading-7">{value}</p>
+    </div>
+  )
+}
+  
+type TourCardProps = {
+  name: string
+  status: string
+  nbrContainers: string
+  nbrDeliveries?: string | null
+  vehicleBrand: string
+  vehicleModel: string
+  vehicleLicensePlate: string
+}
+  
+const TourCard = ({ name, status, nbrContainers, nbrDeliveries, vehicleBrand, vehicleModel, vehicleLicensePlate }: TourCardProps) => {
+  const { i18n: { t } } = useRouteContext({ from: '__root__' })
+  return (
+    <Card role="link">
+      <CardHeader className="p-3">
+        <div className="flex flex-row justify-between">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{name}</h4>
+          <p className="leading-7">{status}</p>
+        </div>
+      </CardHeader>
+      <CardContent className="p-3">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Informations</h4>
+        <TourCardRow label="Nombre de conteneur" value={nbrContainers}/>
+        { nbrDeliveries && <TourCardRow label="Nombre de livraison" value={nbrDeliveries}/> }
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{t('glossary:vehicle')}</h4>
+        <TourCardRow label="Marque" value={vehicleBrand}/>
+        <TourCardRow label="Modèle" value={vehicleModel}/>
+        <TourCardRow label="N° immatriculation" value={vehicleLicensePlate}/>
+      </CardContent>
+      <CardFooter className="p-3 gap-3">
+        { nbrContainers && <Badge className="select-none" variant="secondary">Pick-up</Badge>}
+        { nbrDeliveries && <Badge className="select-none" variant="secondary">{t('glossary:delivery')}</Badge>}
+      </CardFooter>
+    </Card>
+  )
+}
+
+export const ScheduledTourCatalog = ({ scheduledTourDetailsList } : { scheduledTourDetailsList: ReturnType<typeof scheduledTourCatalogMap>}) => {
+  return (
+    <div>
+      {scheduledTourDetailsList.map(scheduledTour => {
+        return <Link key={`scheduledTour-${scheduledTour.id}`} to={`/scheduled-tours/${scheduledTour.id.toString}`}>
+          <TourCard 
+            {...scheduledTour}
+          />
+        </Link>
+      })}
+    </div>
+  )
+}
---
+++ src/routes/_isAuthenticated/scheduled-tours/index.tsx
@@ -0,0 +1,133 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  createFileRoute,
+  useRouteContext,
+  useRouter,
+} from '@tanstack/react-router'
+import {
+  queryOptions,
+  useQueryErrorResetBoundary,
+  useSuspenseQuery,
+} from '@tanstack/react-query'
+import { HeaderLayout } from '@/contexts/shared/presenter/Layouts/HeaderLayout'
+import { Token } from '@/contexts/auth/domain/types/authSharedTypes'
+import { ScheduledTourDetailsRepository } from '@/contexts/TourManagement/domain/ScheduledTourDetailsRepository'
+import { useEffect } from 'react'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/contexts/shared/presenter/components/ui/alert-dialog'
+import { ScheduledTourCatalog } from '@/contexts/TourManagement/presenter/components/ScheduledTourCatalog'
+import { scheduledTourCatalogMap } from '@/contexts/TourManagement/presenter/mappers/scheduledTourCatalogMap'
+
+const createScheduledTourDetailsQueryOptions = (
+  token: Token['token'],
+  driverFetchHisOwnScheduledTourDetailsUseCase: ScheduledTourDetailsRepository['getAllScheduledTourDetailsByAssignedUser'],
+) =>
+  queryOptions({
+    queryKey: ['scheduledTours'],
+    queryFn: () => driverFetchHisOwnScheduledTourDetailsUseCase(token),
+  })
+
+export const Route = createFileRoute('/_isAuthenticated/scheduled-tours/')({
+  loader: async ({
+    context: {
+      queryClient,
+      user,
+      driverFetchHisOwnScheduledTourDetailsUseCase,
+    },
+  }) =>
+    queryClient.ensureQueryData(
+      createScheduledTourDetailsQueryOptions(
+        // @ts-expect-error: temporary ts exception
+        user.token,
+        driverFetchHisOwnScheduledTourDetailsUseCase,
+      ),
+    ),
+  component: RouteComponent,
+  errorComponent: ({ error }) => {
+    const {
+      i18n: { t },
+    } = useRouteContext({ from: '__root__' })
+    const router = useRouter()
+    const queryErrorResetBoundary = useQueryErrorResetBoundary()
+
+    useEffect(() => {
+      // Reset the query error boundary
+      queryErrorResetBoundary.reset()
+    }, [queryErrorResetBoundary])
+
+    return (
+      <AlertDialog defaultOpen>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              {t('common:something.went.wrong')}
+            </AlertDialogTitle>
+            <AlertDialogDescription>{error.message}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>{t('glossary:cancel')}</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                // Invalidate the route to reload the loader, and reset any router error boundaries
+                router.invalidate()
+              }}
+            >
+              {t('common:refresh.app')}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    )
+  },
+})
+
+function RouteComponent() {
+  const {
+    i18n: { t },
+    user,
+    driverFetchHisOwnScheduledTourDetailsUseCase,
+  } = useRouteContext({ from: '__root__' })
+
+  const { data: scheduledTourDetailsList } = useSuspenseQuery(
+    createScheduledTourDetailsQueryOptions(
+      // @ts-expect-error: temporary ts exception
+      user.token,
+      driverFetchHisOwnScheduledTourDetailsUseCase,
+    ),
+  )
+
+  if ('errors' in scheduledTourDetailsList) {
+    throw new Error(scheduledTourDetailsList.errors?.[0]?.message)
+  }
+
+  return (
+    <HeaderLayout
+      breadcrumbItemList={[
+        {
+          link: '/',
+          label: t('glossary:home'),
+          hiddenIfMd: true,
+        },
+        {
+          label: t('common:scheduled.tours'),
+          hiddenIfMd: false,
+        },
+      ]}
+    >
+      <ScheduledTourCatalog
+        scheduledTourDetailsList={scheduledTourCatalogMap(
+          scheduledTourDetailsList,
+        )}
+      />
+    </HeaderLayout>
+  )
+}
