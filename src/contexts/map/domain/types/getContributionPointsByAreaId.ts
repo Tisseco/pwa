a0162501ab29c8caea6f343a171421f1848e2d8a
fansoa <fverin.contactpro@gmail.com>
2025-01-19 16:04:58 +0100

    feat(getContributionPointsByAreaId): add types

---
+++ src/contexts/map/domain/types/getContributionPointsByAreaId.ts
@@ -0,0 +1,5 @@
+import { contributionPoint } from "@/contexts/map/domain/aggregates/contributionPoint"
+
+export type GetContributionPointsByAreaIdPayload = number
+
+export type GetContributionPointsByAreaIdSuccess = contributionPoint
