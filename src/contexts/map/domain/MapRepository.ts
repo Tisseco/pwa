48b5b7d8ea3b59f9865535c857bc6c8d262db0ba
fansoa <fverin.contactpro@gmail.com>
2025-01-19 16:05:26 +0100

    feat(getContributionPointsByAreaId): update MapRepository

--- src/contexts/map/domain/MapRepository.ts
+++ src/contexts/map/domain/MapRepository.ts
@@ -1,7 +1,10 @@
+import { GetContributionPointsByAreaIdPayload, GetContributionPointsByAreaIdSuccess } from "@/contexts/map/domain/types/getContributionPointsByAreaId";
 import { GetNearestContributionPointsByGeoPosPayload, GetNearestContributionPointsByGeoPosSuccess } from "./types/getNearestContributionPointsByGeoPos";
 
 export interface MapRepository {
 
   getNearestContributionPointsByGeoPos({ lat, lng }: GetNearestContributionPointsByGeoPosPayload): Promise<GetNearestContributionPointsByGeoPosSuccess>
+
+  getContributionPointsByAreaId(areaId: GetContributionPointsByAreaIdPayload): Promise<GetContributionPointsByAreaIdSuccess>
 
 }
